ca739ba88e6f655819e69c265407f23ee9951fc7
Esjes <esmeralda160867@hotmail.com>
2019-02-21 23:35:32 +0100

    btje opschonen

--- versie-2/js/calculator-2.js
+++ versie-2/js/calculator-2.js
@@ -2,8 +2,8 @@ buttons = document.getElementsByClassName('button');
 var arrShow = [...buttons];
 var displayInput = document.getElementsByClassName('display-input')[0];
 var displayOutput = document.getElementsByClassName('display-output')[0];
-var reset = document.getElementsByClassName('button-reset')[0];
-var equals = document.getElementsByClassName('button-result')[0];
+var reset = document.getElementsByClassName('reset')[0];
+var equals = document.getElementsByClassName('equals')[0];
 
 arrShow.forEach(function (element) {   
     element.addEventListener("click", showInput);
--- versie-3/js/calculator-3.js
+++ versie-3/js/calculator-3.js
@@ -3,8 +3,8 @@ var arrShow = [...buttons];
 var displayInput = document.getElementsByClassName('display-input')[0];
 var displayOutput = document.getElementsByClassName('display-output')[0];
 var calculator = document.getElementsByClassName('calculator')[0];
-var reset = document.getElementsByClassName('button-reset')[0];
-var equals = document.getElementsByClassName('button-result')[0];
+var reset = document.getElementsByClassName('reset')[0];
+var equals = document.getElementsByClassName('equals')[0];
 
 arrShow.forEach(function (element) {
     element.addEventListener("click", showInput);
